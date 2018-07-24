d17be55471bc8f92fa39c0566fb2e580eb5fed50
Woosun Jang <jin890@yonsei.ac.kr>
2018-07-25 01:02:59 +0900

    updated capital and small letters in units

--- utils/generalutils.py
+++ utils/generalutils.py
@@ -7,8 +7,12 @@ class Unitconverter(object):
 
     @staticmethod
     def unit_convert(value, system, unitin, unitout):
-        dic = {"energy": {"Ry": units.Ry, "eV": units.eV, "J": units.J, "Ha": units.Ha},
-               "length": {"Ang": units.Ang, "nm": units.nm, "m": units.m, "Bohr": units.Bohr},
+        dic = {"energy": {"Ry": units.Ry, "Rydberg": units.Ry, "rydberg": units.Ry, "eV": units.eV,
+                          "electronvolt": units.eV, "J": units.J, "Joule": units.J, "joule": units.J,
+                          "Ha": units.Ha, "Hartree": units.Ha, "hartree": units.Ha},
+               "length": {"Ang": units.Ang, "angstrom": units.Ang, "ang": units.Ang, "nanometer": units.nm,
+                          "nm": units.nm, "Nanometer": units.nm, "m": units.m, "meter": units.m, "Meter": units.m,
+                          "Bohr": units.Bohr, "bohr": units.Bohr},
                }
         try:
             return value * (dic[system][unitin] / dic[system][unitout])
