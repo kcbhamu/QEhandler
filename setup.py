20f71648516055a04d43248f8d67fc0ed2ebdacd
Woosun Jang <jin890@yonsei.ac.kr>
2018-07-26 01:52:48 +0900

    setup.py generated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QEhandler',
+    version='0.2',
+    packages=find_packages(exclude=['test*']),
+    url='https://github.com/woosunjang/QEhandler',
+    license='GPL-3.0',
+    author='Woosun Jang',
+    author_email='jin890@yonsei.ac.kr',
+    description='',
+    package_data={"pwscf": ["*.yaml"]},
+    include_package_data=True
+)
