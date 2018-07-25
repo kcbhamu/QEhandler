3ee65444abaab75f10e30dc3c075f24d7327c90d
Woosun Jang <jin890@yonsei.ac.kr>
2018-07-26 00:26:47 +0900

    initial commit

---
+++ pwscf/pw_output.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals, print_function
+from collections import OrderedDict
+from utils.generalutils import ScientificConstants, Unitconverter
+import numpy as np
+import yaml
+import spglib
+
+
+class PWout(object):
+
+    def __init__(self, infile, calculation=None):
+        self.infile = infile
+        self.memory = None
+        self.calculation = None or calculation
+        self.status = None
+        self.energy = None
+        self.structure = None
+        return
+
+    def read_file(self):
+        with open(self.infile, "r") as file:
+            f = file.readlines()
+            # for line in f:
+            #     return
+
+        return
