ba9ff811a92c7d8eada321e4dac84d421a7b1e3d
Woosun Jang <jin890@yonsei.ac.kr>
2018-07-26 09:34:01 +0900

    revised to exclude test folders

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='QEhandler',
     version='0.2',
-    packages=find_packages(exclude=['test*']),
+    packages=find_packages(exclude=['*test*']),
     url='https://github.com/woosunjang/QEhandler',
     license='GPL-3.0',
     author='Woosun Jang',
