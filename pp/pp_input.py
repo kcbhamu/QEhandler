e722bdaf9c8a63edb6f17c8c2404779c8e2a47e0
Woosun Jang <jin890@yonsei.ac.kr>
2018-07-28 13:38:29 +0900

    initial commit

---
+++ pp/pp_input.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals, print_function
+from collections import OrderedDict
+from utils.generalutils import ScientificConstants, Unitconverter
+import os
+import sys
+import yaml
+import numpy as np
+
+class PPin(object):
+
+    def __init__(self):
+        self.inputpp = OrderedDict()
+        self.plot = OrderedDict()
+        with open(os.path.join(os.path.dirname(__file__), "input_tags.yaml"), "r") as y:
+            self.taglist = ordered_load(y, yaml.SafeLoader)[0]
+
